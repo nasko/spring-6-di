1dd2487d2fa9aa4c3712c88061e5db11a94a6e1a
Atanas Vasilev <atanas.vasilev@primeholding.com>
2025-04-10 00:03:23 +0300

    Refactor the three controllers to utilize Spring-managed DI, actually achieving IoC

--- src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
@@ -3,16 +3,14 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class PropertyInjectedControllerTest {
 
+    @Autowired
     PropertyInjectedController controller;
-
-    @BeforeEach
-    void setUp() {
-        controller = new PropertyInjectedController();
-        controller.greetingService = new GreetingServiceImpl();
-    }
 
     @Test
     void sayHello() {
--- src/test/java/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
@@ -3,16 +3,14 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class SetterInjectedControllerTest {
 
+    @Autowired
     SetterInjectedController controller;
-
-    @BeforeEach
-    void setUp() {
-        controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
-    }
 
     @Test
     void sayHello() {
