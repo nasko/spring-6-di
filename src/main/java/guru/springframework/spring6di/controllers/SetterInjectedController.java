0acc79ed7197491ad8453b7f791e25633ddc01d2
Atanas Vasilev <atanas.vasilev@primeholding.com>
2025-04-10 00:45:23 +0300

    Introduce service qualifiers

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -2,15 +2,17 @@ package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
-        System.out.println("Setter method called");
+        System.out.println("Setter method called!!!");
         this.greetingService = greetingService;
     }
 
